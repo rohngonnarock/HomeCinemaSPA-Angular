9f3c68f8d069b657a4f868c01d5fe6c5ff01d137
rahulpd <rahulpd@goldengekko.com>
2015-09-18 11:18:31 +0530

    Package addition

--- HomeCinema.Data/EntityBaseConfiguration.cs
+++ HomeCinema.Data/EntityBaseConfiguration.cs
@@ -1,17 +1,19 @@
-﻿using System;
+﻿using HomeCinema.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeCinema.Data
 {
-    public class EntityBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class,
-IEntityBase
+    public class EntityBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class, IEntityBase
     {
         public EntityBaseConfiguration()
         {
             HasKey(e => e.ID);
         }
-    }
+    }
+
 }
--- HomeCinema.Data/HomeCinemaContext.cs
+++ HomeCinema.Data/HomeCinemaContext.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using HomeCinema.Entities;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
