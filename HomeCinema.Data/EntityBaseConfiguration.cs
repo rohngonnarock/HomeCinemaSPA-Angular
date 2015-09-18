fee727ec3d71c4a3a3976ed8ffc17c7a57804275
rahulpd <rahulpd@goldengekko.com>
2015-09-18 11:05:40 +0530

    Initial commit

---
+++ HomeCinema.Data/EntityBaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeCinema.Data
+{
+    public class EntityBaseConfiguration<T> : EntityTypeConfiguration<T> where T : class,
+IEntityBase
+    {
+        public EntityBaseConfiguration()
+        {
+            HasKey(e => e.ID);
+        }
+    }
+}
